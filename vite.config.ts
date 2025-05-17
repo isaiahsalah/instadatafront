37aa23a680f2d2bdcf0343b70956a99fafd9e85f
isaiahsalah <isasalas145@gmail.com>
2025-05-17 11:35:32 -0400

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import {defineConfig} from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: 5175, // Cambia este valor por el puerto que quieras usar
+  },
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
